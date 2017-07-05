444e7a2a2bdb77e6e62e61f4faafe8b3f6c2488c
Oğuzhan YILMAZ <yilmazoguz94@gmail.com>
2017-07-05 23:02:51 +0300

    add facebook login

---
+++ main/java/sample/oguz/com/firebaseauthentication/MainActivity.java
@@ -0,0 +1,207 @@
+package sample.oguz.com.firebaseauthentication;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+@Fullscreen
+public class MainActivity extends Activity implements GoogleApiClient.OnConnectionFailedListener{
+
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+    private GoogleApiClient mGoogleApiClient;
+
+    @ViewById(R.id.userEmailTxt)
+    EditText userEmail;
+
+    @ViewById(R.id.userPasswordTxt)
+    EditText userPassword;
+
+    @ViewById(R.id.googleSignBtn)
+    Button googleSignBtn;
+
+    @ViewById(R.id.progressBar)
+    ProgressBar progressBar;
+
+    @ViewById(R.id.signupBtn)
+    Button signupBtn;
+
+    @ViewById(R.id.normalSignBtn)
+    Button normalSignBtn;
+
+
+    @AfterViews
+    void onCreate () {
+        auth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        //Geçerli bir yetkilendirme olup olmadığını kontrol ediyoruz.
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(MainActivity.this,HomeActivity_.class));
+        }
+
+        //FacebookAuth
+
+    }
+
+    @Click(R.id.connectWithFbButton)
+    void connectWithFacebook() {
+        startActivity(new Intent(MainActivity.this, FacebookLogin.class));
+    }
+    @Click(R.id.googleSignBtn)
+    void signin() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In basarili oldugunda Firebase ile yetkilendir
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+
+            } else {
+                // Google Sign In hatası.
+                //Log.e(TAG, "Google Sign In hatası.");
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        // Log.d(TAG, "firebaseAuthWithGooogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //             Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+
+                        if (!task.isSuccessful()) {
+                            //               Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(MainActivity.this, "Yetkilendirme hatası.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(MainActivity.this, HomeActivity_.class));
+                            finish();
+                        }
+                    }
+                });}
+    @Click(R.id.normalSignBtn)
+    void normalSign () {
+        auth = FirebaseAuth.getInstance();
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Lütfen emailinizi giriniz", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Parola girilmemiş ise kullanıcıyı uyarıyoruz.
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Lütfen parolanızı giriniz", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Firebase üzerinde kullanıcı doğrulamasını başlatıyoruz
+        //Eğer giriş başarılı olursa task.isSuccessful true dönecek ve MainActivity e geçilecek
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(MainActivity.this,HomeActivity_.class));
+                        }
+                        else {
+                            Log.e("Giriş Hatası",task.getException().getMessage());
+                        }
+                    }
+                });
+
+
+    }
+    @Click(R.id.signupBtn)
+    void signup () {
+        String email = userEmail.getText().toString();
+        String parola = userPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),"Lütfen emailinizi giriniz",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(parola)){
+            Toast.makeText(getApplicationContext(),"Lütfen parolanızı giriniz",Toast.LENGTH_SHORT).show();
+        }
+        if (parola.length()<6){
+            Toast.makeText(getApplicationContext(),"Parola en az 6 haneli olmalıdır",Toast.LENGTH_SHORT).show();
+        }
+
+        //FirebaseAuth ile email,parola parametrelerini kullanarak yeni bir kullanıcı oluşturuyoruz.
+        auth.createUserWithEmailAndPassword(email,parola)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        //İşlem başarısız olursa kullanıcıya bir Toast mesajıyla bildiriyoruz.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Yetkilendirme Hatası",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        //İşlem başarılı olduğu takdir de giriş yapılıp MainActivity e yönlendiriyoruz.
+                        else {
+                            startActivity(new Intent(MainActivity.this, HomeActivity_.class));
+                            finish();
+                        }
+
+                    }
+                });
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
